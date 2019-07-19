cff4fbdf2f1c837fb1fa693a806b9dc3ea6a9d3f
Jason Belgard <belgard.jason@gmail.com>
2019-07-19 11:46:02 -0500

    Add Service Impl

--- finished/src/main/java/com/lambdaschool/todo/repo/TodoRepository.java
+++ finished/src/main/java/com/lambdaschool/todo/repo/TodoRepository.java
@@ -1,7 +1,13 @@
 package com.lambdaschool.todo.repo;
 
 import com.lambdaschool.todo.model.Todo;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TodoRepository extends CrudRepository<Todo, Long> {
+    @Query(value = "SELECT * FROM todo WHERE userid=:userid", nativeQuery = true)
+    List<Todo> getAllByID(long id);
 }
