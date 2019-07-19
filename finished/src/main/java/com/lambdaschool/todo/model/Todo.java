a063eab44bf7880d35a27ffd6ad036594caddbd5
Jason Belgard <belgard.jason@gmail.com>
2019-07-19 11:51:34 -0500

    Controllers and security config files

--- finished/src/main/java/com/lambdaschool/todo/model/Todo.java
+++ finished/src/main/java/com/lambdaschool/todo/model/Todo.java
@@ -6,9 +6,9 @@ import java.util.Date;
 @Entity
 @Table(name = "todo")
 public class Todo extends Auditable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(nullable = false)
     private long todoid;
 
     @Column(nullable = false)
@@ -20,7 +20,6 @@ public class Todo extends Auditable {
 
     @ManyToOne
     @JoinColumn(name = "userid")
-    @Column(nullable = false)
     private User user;
 
     public Todo() {
--- finished/src/main/java/com/lambdaschool/todo/repo/TodoRepository.java
+++ finished/src/main/java/com/lambdaschool/todo/repo/TodoRepository.java
@@ -2,12 +2,19 @@ package com.lambdaschool.todo.repo;
 
 import com.lambdaschool.todo.model.Todo;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
     @Query(value = "SELECT * FROM todo WHERE userid=:userid", nativeQuery = true)
     List<Todo> getAllByID(long id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM todo WHERE userid = :userid", nativeQuery = true)
+    void deleteAllByUserId(long userid);
 }
