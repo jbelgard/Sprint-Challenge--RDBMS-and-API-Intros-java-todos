6f973880171eb86fea0c18fcda861edccd300ddc
Jason Belgard <belgard.jason@gmail.com>
2019-07-19 11:33:12 -0500

    Models

---
+++ finished/src/main/java/com/lambdaschool/todo/model/Todo.java
@@ -0,0 +1,81 @@
+package com.lambdaschool.todo.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "todo")
+public class Todo extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long todoid;
+
+    @Column(nullable = false)
+    private String description;
+
+    private Date datestarted;
+
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    @Column(nullable = false)
+    private User user;
+
+    public Todo() {
+    }
+
+    public Todo(String description, Date datestarted, boolean completed) {
+        this.description = description;
+        this.datestarted = datestarted;
+        this.completed = completed;
+    }
+
+    public Todo(String description, Date datestarted, boolean completed, User user) {
+        this.description = description;
+        this.datestarted = datestarted;
+        this.completed = completed;
+        this.user = user;
+    }
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDatestarted() {
+        return datestarted;
+    }
+
+    public void setDatestarted(Date datestarted) {
+        this.datestarted = datestarted;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
